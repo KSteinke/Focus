5ac9b1397abaa339990b23d3feefc7517cd72a30
KSteinke <kacpersteinkesb@gmail.com>
2025-08-02 21:41:52 +0200

    ViewUpdate

--- FocusApp/src/app/app.ts
+++ FocusApp/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('FocusApp');
+  backgroundUrl: string = 'assets/Tree.png';
 }
