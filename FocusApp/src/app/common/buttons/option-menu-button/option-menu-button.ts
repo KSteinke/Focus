608c0a4f902c36017a7d695f6fcf383b7292ffbf
KSteinke <kacpersteinkesb@gmail.com>
2025-08-08 00:21:13 +0200

    Dynamics Icons

--- FocusApp/src/app/common/buttons/option-menu-button/option-menu-button.ts
+++ FocusApp/src/app/common/buttons/option-menu-button/option-menu-button.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-option-menu-button',
   imports: [],
   templateUrl: './option-menu-button.html',
   styleUrl: './option-menu-button.scss'
 })
 export class OptionMenuButton {
+
+  @Input() Name: string = "";
+  @Input() IconPath: string = "";
+
+  @Output() clickEvent = new EventEmitter<string>(true);
+
+  click()
+  {
+    this.clickEvent.emit(this.Name);
+  }
 
 }
