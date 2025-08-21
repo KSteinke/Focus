ab70f7e315b00783d9c82e9fc454ff143290f236
KSteinke <kacpersteinkesb@gmail.com>
2025-08-21 21:29:07 +0200

    Time format persistance

--- FocusApp/src/app/core/services/main-timer-service/main-timer-serivice.ts
+++ FocusApp/src/app/core/services/main-timer-service/main-timer-serivice.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { LocalStorageService } from '../local_storage_service/local-storage-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainTimerSerivice {
+  constructor(private localStorageService:LocalStorageService)
+  {
+    let timeFormatIs24h : boolean | null = this.localStorageService.getItem(this.timeFormatKeyValue);
+    timeFormatIs24h != null ? this.timeFormatIs24hSubject.next(timeFormatIs24h) : this.timeFormatIs24hSubject.next(false);
+  }
+  private timeFormatKeyValue = "timeFormat";
 
   private timeFormatIs24hSubject = new BehaviorSubject<boolean>(false);
   public TimeFormatIs24hObservable = this.timeFormatIs24hSubject.asObservable();
 
-  ngOnInit()
-  {
-    this.timeFormatIs24hSubject.next(false);
-  }
-
   public SetTimeFormat(format : string)
   {
-    format === "12h" ? this.timeFormatIs24hSubject.next(false) : this.timeFormatIs24hSubject.next(true);
+    let timeFormatIs24h : boolean;
+    format === "12h" ? timeFormatIs24h = false : timeFormatIs24h = true;
+    this.localStorageService.setItem(this.timeFormatKeyValue, timeFormatIs24h);
+    this.timeFormatIs24hSubject.next(timeFormatIs24h);
   }
   
 }
