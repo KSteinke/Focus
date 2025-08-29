61c3cc3713065fbe350792f64cf4b59d2ace0da1
KSteinke <kacpersteinkesb@gmail.com>
2025-08-29 18:22:11 +0200

    Sub pages added, background moved to dashboard

--- FocusApp/src/app/app.ts
+++ FocusApp/src/app/app.ts
@@ -1,33 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
-import { NgStyle } from '@angular/common';
-import { Background, BackgroundService } from './core/services/background_service/background-service';
-import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard, NgStyle],
+  imports: [RouterOutlet, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  constructor(private backgroundService: BackgroundService) {}
-
-  public backgroundSubscription!: Subscription;
-
-  protected readonly title = signal('FocusApp');
-  background!: Background;
-
-  ngOnInit()
-  {
-      this.backgroundSubscription = this.backgroundService.currentBackground.subscribe(data => {
-      this.background = data;
-    });
-  }
-
-  ngOnDestroy() {
-    this.backgroundSubscription.unsubscribe();
-  }
 
 }
--- FocusApp/src/app/features/dashboard/dashboard.ts
+++ FocusApp/src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component, signal  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Quote } from './quote/quote';
 import { MainTimer } from './main-timer/main-timer';
@@ -6,20 +6,40 @@ import { SoundButton } from '../../common/buttons/sound-button/sound-button';
 import { OptionsButton } from '../../common/buttons/options-button/options-button';
 import { FullScreenButton } from '../../common/buttons/full-screen-button/full-screen-button';
 import { OptionsDashboard } from './options-dashboard/options-dashboard';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgStyle } from '@angular/common';
 import { PomodoroButton } from '../../common/buttons/pomodoro-button/pomodoro-button';
 import { PomodoroTimer } from './pomodoro-timer/pomodoro-timer';
+import { Background, BackgroundService } from '../../core/services/background_service/background-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
-  imports: [RouterOutlet, Quote, MainTimer, SoundButton, OptionsButton, FullScreenButton, OptionsDashboard, CommonModule, PomodoroButton, PomodoroTimer],
+  imports: [RouterOutlet, Quote, MainTimer, SoundButton, OptionsButton, FullScreenButton, OptionsDashboard, CommonModule, PomodoroButton, PomodoroTimer, NgStyle],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
   private _isOptionsDashboardOpen: Boolean = false;
   private currentTimer : TimerType = TimerType.Normal;
+
+  public backgroundSubscription!: Subscription;
+  
+    protected readonly title = signal('FocusApp');
+    background!: Background;
+
+  constructor(private backgroundService: BackgroundService){}
+
+  ngOnInit()
+  {
+      this.backgroundSubscription = this.backgroundService.currentBackground.subscribe(data => {
+      this.background = data;
+    });
+  }
+
+  ngOnDestroy() {
+    this.backgroundSubscription.unsubscribe();
+  }
 
   private set CurrentTimer(value: TimerType)
   {
